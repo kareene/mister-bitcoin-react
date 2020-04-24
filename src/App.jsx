fc598c4bee6987ba969c2a297338129730acec19
Kareene Komet <kareene@kareene.com>
2020-04-24 21:29:07 +0300

    switched to HashRouter for deply to gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 import PrivateRoute from './components/PrivateRoute';
@@ -21,11 +21,11 @@ function App() {
         <main className="ContentContainer">
           <Switch>
             <PrivateRoute path="/" exact component={HomePage} />
-            <Route path="/signup" component={SignupPage} />
             <PrivateRoute path="/contact" exact component={ContactPage} />
             <PrivateRoute path="/contact/edit/:id?" component={ContactEditPage} />
             <PrivateRoute path="/contact/:id" component={ContactDetailsPage} />
             <PrivateRoute path="/statistic" component={StatisticPage} />
+            <Route path="/signup" component={SignupPage} />
             <Redirect from="*" to="/" />
           </Switch>
         </main>
